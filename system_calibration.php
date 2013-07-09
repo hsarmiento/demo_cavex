6c33e56554ee15df2b459667a4ba6d5c87b40e1e
Hernan Sarmiento <hernan.sarmientoa@gmail.com>
2013-07-09 17:16:05 -0400

    Se aplica diseño a system calibration

--- system_calibration.php
+++ system_calibration.php
@@ -4,7 +4,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/demo_cavex/'.'header.php');
 require_once($aRoutes['paths']['config'].'bs_model.php');
 $oLogin = new BSLogin();
 $oLogin->IsLogged("admin");
-
+$save = false;
 
 $form = $_POST['radio'];
 if(!empty($_POST['save'])){
@@ -13,28 +13,31 @@ if(!empty($_POST['save'])){
 		$query_save = "INSERT INTO parametros(rms,porcentaje_rms,desviacion_standard, porcentaje_sd, radio_id)values(".$value['rms_normal'].",".$value['rms_max_normal'].", ".$value['sd_normal'].", ".$value['sd_max_normal'].", ".$value['radio_id'].") on duplicate key update rms=".$value['rms_normal'].", porcentaje_rms=".$value['rms_max_normal'].", desviacion_standard=".$value['sd_normal'].", porcentaje_sd=".$value['sd_max_normal'].";";
 		// echo $query_save;
 		$oParametros->Select($query_save);
+		$save = true;
 	}
 }
 
-// echo '<pre>';
-// print_r($_POST);
-// echo '</pre>';
 $oRadios = new BSModel();
-$query_radios = "SELECT * from radios where estado = 1;";
+$query_radios = "select radios.id as radio_id, radios.mac as mac, parametros.rms as rms, parametros.porcentaje_rms as porcentaje_rms, parametros.desviacion_standard as desviacion_standard, parametros.porcentaje_sd as porcentaje_sd from radios left join parametros on radios.id = parametros.radio_id where radios.estado = 1;";
 $aRadios = $oRadios->Select($query_radios);
 
 ?>
 
 
 <div class="container-body container-calibration">
+	<?php if($save === true) { ?>
+		<div class="alert alert-success" style="text-align:center;">
+	    	Saved settings
+	  	</div>
+	<?php } ?>
   <h2>System Calibration</h2>
-  	  <div class="span11 contenedor">
+  	  <div class="span12 contenedor">
   	  	<?php if(count($aRadios) > 0) {?>
 	  	  	<form name="set_parametros" action="system_calibration.php" id="set_parametros_form" method="post" enctype="multipart/form-data">
 		  	  	<?php foreach ($aRadios as $i=>$radio) { ?>
-		  	  		<input type="hidden" value="<?=$radio['id']?>" name="radio[<?=$i?>][radio_id]">
-		  	  		<div class="span10 offset1 calibration-radio" >
-				    	<span style="font-size:18px;"><strong>Radio <?=$i+1?> - <?=$radio['mac']?></strong></span></br></br>
+		  	  		<input type="hidden" value="<?=$radio['radio_id']?>" name="radio[<?=$i?>][radio_id]">
+		  	  		<div class="span11 offset1 calibration-radio">
+				    	<span style="font-size:18px;"><strong>Radio <?=$i+1?>, MAC: <?=$radio['mac']?></strong></span></br></br>
 				    	<div class="span1 data-type"><strong>RMS</strong></div>
 				    	<div class="span9">	
 							<div class="controls controls-row">
@@ -45,8 +48,8 @@ $aRadios = $oRadios->Select($query_radios);
 							</div>
 							<div class="controls controls-row">
 							    <strong><span id="rms_calibration<?=$i?>" class="current-value" ></span></strong>
-							    <input type="text" class="calibration first-input required" name="radio[<?=$i?>][rms_normal]"/>
-							    <input type="text" class="calibration second-input required" name="radio[<?=$i?>][rms_max_normal]" />
+							    <input type="text" class="calibration first-input required" name="radio[<?=$i?>][rms_normal]" value="<?=$radio['rms']?>"/>
+							    <input type="text" class="calibration second-input required" name="radio[<?=$i?>][rms_max_normal]" value="<?=$radio['porcentaje_rms']?>"/>
 							    <input type="text" class="calibration third-input required" name="radio[<?=$i?>][rms_ropping]" />
 							</div>    		
 				    	</div>
@@ -61,8 +64,8 @@ $aRadios = $oRadios->Select($query_radios);
 							</div>
 							<div class="controls controls-row">
 							    <strong><span id="rms_calibration<?=$id?>" class="current-value" ></span></strong>
-							    <input type="text" class="calibration first-input required" name="radio[<?=$i?>][sd_normal]" value="<?=$aParametros[0]['rms']?>"/>
-							    <input type="text" class="calibration second-input required" name="radio[<?=$i?>][sd_max_normal]" value="<?=$aParametros[0]['porcentaje_rms']?>"/>
+							    <input type="text" class="calibration first-input required" name="radio[<?=$i?>][sd_normal]" value="<?=$radio['desviacion_standard']?>"/>
+							    <input type="text" class="calibration second-input required" name="radio[<?=$i?>][sd_max_normal]" value="<?=$radio['porcentaje_sd']?>"/>
 							    <input type="text" class="calibration third-input required" name="radio[<?=$i?>][sd_ropping]" value="<?=$aParametros[0]['porcentaje_rms']?>"/>
 							</div>						   		
 				    	</div>
