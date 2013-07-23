7073be1123632224a349e392e7fbb5bbd69c8731
Hernan Sarmiento <hernan.sarmientoa@gmail.com>
2013-07-23 17:02:00 -0400

    Se corrige add radios

--- add_radio.php
+++ add_radio.php
@@ -9,8 +9,8 @@ $is_save = false;
 
 $form = $_POST;
 if(!empty($form['save_radio'])){
-	if(!empty($form['mac1']) && !empty($form['mac2']) && !empty($form['mac3']) && !empty($form['mac4']) && !empty($form['mac5']) && !empty($form['mac6'])){
-		$MAC = $form['mac1'].":".$form['mac2'].":".$form['mac3'].":".$form['mac4'].":".$form['mac5'].":".$form['mac6'];
+	if(!empty($form['mac1']) && !empty($form['mac2']) && !empty($form['mac3']) && !empty($form['mac4'])){
+		$MAC = $form['mac1'].$form['mac2'].$form['mac3'].$form['mac4'];
 		$oRadio = new BSModel();
 		$query_new_radio = "INSERT INTO radios(mac)values('".$MAC."');";
 		echo $query_new_radio;
@@ -33,12 +33,10 @@ if(!empty($form['save_radio'])){
 		<div class="span6 form-div-radio">
 			<form class="form-inline" id="add_radio_form" method="post" action="add_radio.php" enctype="multipart/form-data">
 			  <label for="mac1"><strong>Mac address</strong></label>
-			  <input type="text" class="input-mini span1" id="mac1" name="mac1"><b>:</b>
-			  <input type="text" class="input-mini span1" id="mac2" name="mac2"><b>:</b>
-			  <input type="text" class="input-mini span1" id="mac3" name="mac3"><b>:</b>
-			  <input type="text" class="input-mini span1" id="mac4" name="mac4"><b>:</b>
-			  <input type="text" class="input-mini span1" id="mac5" name="mac5"><b>:</b>
-			  <input type="text" class="input-mini span1" id="mac6" name="mac6">
+			  <input type="text" class="input-mini span1" id="mac1" name="mac1" maxlength="4"><b>:</b>
+			  <input type="text" class="input-mini span1" id="mac2" name="mac2" maxlength="4"><b>:</b>
+			  <input type="text" class="input-mini span1" id="mac3" name="mac3" maxlength="4"><b>:</b>
+			  <input type="text" class="input-mini span1" id="mac4" name="mac4" maxlength="4">
 			  <input type="submit" class="btn btn-primary" id="save-radio" name="save_radio" value="Save">
 			</form>
 		</div>
