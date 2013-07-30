e1b8f871705cb177149cfe1191c96ff61ba0a10e
Hernan Sarmiento <hernan.sarmientoa@gmail.com>
2013-07-30 13:03:08 -0400

    Se agrega estado de bateria en listado de radios

--- radios.php
+++ radios.php
@@ -8,9 +8,8 @@ $oLogin = new BSLogin();
 $oLogin->IsLogged("admin");
 
 $oModel = new BSModel();
-$query_radios = "SELECT * from radios order by id asc;";
+$query_radios = "SELECT radios.id as radio_id, radios.mac as mac, radios.estado as estado, bateria_radio.conectada as bateria_conectada, bateria_radio.conectada as estado_bateria, bateria_radio.carga as carga from radios left join bateria_radio on radios.id = bateria_radio.radio_id order by radios.id asc;";
 $aRadios = $oModel->Select($query_radios);
-
 
 $is_save = $_GET['save_radio'];
 // print_r($aRadios);
@@ -32,6 +31,8 @@ $is_save = $_GET['save_radio'];
 				<tr>
 			      <th>MAC address</th>
 			      <th>Status</th>
+			      <th>Battery</th>
+			      <th>Battery Charge</th>
 			      <th>Actions</th>
 			    </tr>
 			</thead>
@@ -42,10 +43,30 @@ $is_save = $_GET['save_radio'];
 				      <td><?=$radio['mac']?></td>
 				      <?php if($radio['estado'] == 0){ ?>
 							<td><span style="color:red;">Disconnected</span></td>
-
 				      <?php } elseif ($radio['estado'] == 1) { ?>
 				      		<td><span style="color:green">Connected</span></td>
+				      <?php }elseif ($radio['estado'] == -1) { ?>	       
+				      		<td><span style="color:blue">Conectala rey</span></td>
 				      <?php }?>
+				      <td>
+				      	<?php
+				      		if($radio['estado'] == 1){ ?>	
+								<?php 
+				      			if($radio['bateria_conectada'] == 1){ ?>
+		      						<span style="color:green;">Connected</span>
+				      			<?php }
+				      			elseif($radio['bateria_conectada'] == 0){ ?>
+									<span style="color:red;">Disconnected</span>
+				      			<?php }
+				      		}	 ?>	
+			      		</td>
+			      		<td>
+			      			<?php if($radio['estado'] == 1){ 
+				      			if(!empty($radio['bateria_conectada'])){ 
+				      					echo $radio['carga'].'%';
+			      					 }	
+					      		}?>
+				      	</td>	
 				      <td>
 				      		<div class="edit_div">
 				      			<a href="edit_radio.php?radio_id=<?=$radio['id']?>&n_radio=<?=$i?>">
